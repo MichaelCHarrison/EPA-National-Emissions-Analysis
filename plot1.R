28843f9cdeaa838b07b7e65be0040b9eb7687715
MichaelCHarrison <michael.harrison2010@gmail.com>
2017-02-16 23:17:16 -0800

    plot1 commit

--- plot1.R
+++ plot1.R
@@ -1,6 +1,17 @@
 plot1 <- function(){
-        summary <- readRDS("summarySCC_PM25.rds")
+        library(dplyr)
+        summary <- tbl_df(readRDS("summarySCC_PM25.rds"))
         classcodes <- readRDS("Source_Classification_Code.rds")
         
-        plot()
+        summary <- summary %>%
+                group_by(year) %>%
+                summarise(totalEmissions = sum(Emissions))
+        
+        plot(x = summary$year, y = summary$totalEmissions, 
+             main = "Total US Emisisons: 1999-2008",
+             xlab = "Year", ylab = "Total Emissions (Tons)",
+             pch = 19, col = "red",
+             xlim = c(1999, 2008)
+             )
+        lines(x = summary$year, y = summary$totalEmissions, type = 'l')
 }
